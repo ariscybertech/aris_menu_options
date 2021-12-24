86dca367f020e02798114c400ae48b2e3a773a1e
ariscybertech <ariscybertech@gmail.com>
2021-12-24 18:35:00 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Flutter Menu Options UI
+
+[YouTube Speed Code](https://youtu.be/1oegwxUIrkc)
+
+[Design Credit](https://prnt.sc/p356zf)
+
---
+++ android/app/src/main/kotlin/com/example/aris_menu_options/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aris_menu_options
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
